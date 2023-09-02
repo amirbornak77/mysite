9073bf444cf03a67346944109482645b07531890
amirbornak77 <131398939+amirbornak77@users.noreply.github.com>
2023-09-02 11:45:17 -0700

    fixing all urls
    fixing all urls for part of templates with app_name and {% url '' %}

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from website.views import *
 
+app_name = "website"
 urlpatterns = [
     path('',index_view, name='index'),
     path('about/',about_view, name='about'),
