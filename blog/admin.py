efdbb2f4b8d1a6bcc3195800a7de45eac5ad33fb
amirbornak77 <131398939+amirbornak77@users.noreply.github.com>
2023-09-12 10:37:23 -0700

    new attribute is just update
    linking , querysets, all updated

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_views', 'status', 'publsihed_date', 'created_date')
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
     list_filter = ('status',)
     search_fields = ['title', 'content']
     
--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     #author
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
-    publsihed_date = models.DateTimeField(null=True)
+    published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     class Meta:
